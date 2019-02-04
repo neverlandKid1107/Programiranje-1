db4a6ee1ebe7ae4f7d2c9c89d6cb5674e6b72874
neverlandKid1107 <urban.fila10@gmail.com>
2019-02-04 21:11:19 +0100

    Create Vsota2.java

---
+++ Vsota2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Vsota2{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int st1 = sc.nextInt();
+    int st2 = sc.nextInt();
+    int vsota = 0;
+
+    vsota = st1 + st2;
+
+    System.out.println(st1+" + "+st2+" = "+vsota);
+  }
+}
